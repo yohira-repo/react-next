9fecf869c61a8d8bd647c414e8d5d55cf4fd7d5b
yohira <39988933+yohira-repo@users.noreply.github.com>
2022-08-10 08:56:39 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./styles.css";
+import { useState } from "react";
+import { ChildArea } from "./ChildArea";
+
+export default function App() {
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (e) => setText(e.target.value);
+  const onClickOpen = () => setOpen(!open);
+
+  return (
+    <div className="App">
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} />
+    </div>
+  );
+}
---
+++ src/ChildArea.jsx
@@ -0,0 +1,12 @@
+export const ChildArea = (props) => {
+  const { open } = props;
+  return (
+    <>
+      {open ? (
+        <div>
+          <p>子コンポーネント</p>
+        </div>
+      ) : null}
+    </>
+  );
+};
