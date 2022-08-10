04df8ec8a2f6e6d53922bb887ab504b89eadfb45
yohira <39988933+yohira-repo@users.noreply.github.com>
2022-08-10 11:14:03 +0900

    next update 3.11

--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,12 +1,25 @@
-export const ChildArea = (props) => {
+import { memo } from "react";
+
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+
+export const ChildArea = memo((props) => {
+  console.log("ChildAreaがレンダリングされた");
   const { open } = props;
+  const data = [...Array(2000).keys()];
+  console.log(data);
+
   return (
     <>
       {open ? (
-        <div>
+        <div style={style}>
           <p>子コンポーネント</p>
+          <button>閉じる</button>
         </div>
       ) : null}
     </>
   );
-};
+});
